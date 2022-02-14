f3dd6209885ebaf3395a4faee9b3f778a4119cd9
anuragkolluru <89211878+anuragkolluru@users.noreply.github.com>
2022-02-13 21:05:28 -0600

    added file

---
+++ Kolluru_Calculator/Kolluru_Calculator/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Kolluru_Calculator
+//
+//  Created by Kolluru,Anurag on 2/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func allClear(_ sender: UIButton) {
+    }
+    @IBAction func clear(_ sender: UIButton) {
+    }
+    @IBAction func plusAndMinu(_ sender: UIButton) {
+    }
+    @IBAction func division(_ sender: UIButton) {
+    }
+    @IBAction func seven(_ sender: UIButton) {
+    }
+    @IBAction func eight(_ sender: UIButton) {
+    }
+    @IBAction func nine(_ sender: UIButton) {
+    }
+    @IBAction func multiply(_ sender: UIButton) {
+    }
+    @IBAction func four(_ sender: UIButton) {
+    }
+    @IBAction func five(_ sender: UIButton) {
+    }
+    @IBAction func six(_ sender: UIButton) {
+    }
+    @IBAction func minus(_ sender: UIButton) {
+    }
+    @IBAction func one(_ sender: UIButton) {
+    }
+    @IBAction func two(_ sender: UIButton) {
+    }
+    @IBAction func three(_ sender: UIButton) {
+    }
+    @IBAction func plus(_ sender: UIButton) {
+    }
+    @IBAction func zero(_ sender: UIButton) {
+    }
+    @IBAction func decimal(_ sender: UIButton) {
+    }
+    @IBAction func reminder(_ sender: UIButton) {
+    }
+    @IBAction func equalsTo(_ sender: UIButton) {
+    }
+    
+    
+
+
+}
+
